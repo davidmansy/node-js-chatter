ce03c51b237f6b06331135c7a7c6dd0a65760dbe
David Mansy <david.mansy@me.com>
2013-09-21 19:22:42 +0200

    Modify chatter-server.js

--- chatter-server.js
+++ chatter-server.js
@@ -1,8 +1,13 @@
 var socket = require('socket.io');
 var express = require('express');
 var app = express.createServer();
-var io = socket.listen(app);
 
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+var io = socket.listen(app);
 io.socket.on('connection', function(client) {
 	console.log("Client connection");
 });
