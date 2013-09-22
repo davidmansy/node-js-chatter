d13f11e3a0266aef2add56901f2f045e7f9f8048
David Mansy <david.mansy@me.com>
2013-09-22 11:58:22 +0200

    Update based on socket.io website

--- chatter-server.js
+++ chatter-server.js
@@ -1,12 +1,10 @@
 //socket.io requires a server so transfrom the express app to a server
-var express = require("express")
-	,	app = express()
-  , http = require('http')
-  , server = http.createServer(app)
+var app = require("express")()
+  , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
 //Express
-app.listen(8080);
+server.listen(8080);
 
 //Express
 app.get('/', function (req, res) {
