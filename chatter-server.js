8c96ca15a031751081525b92fd4d24952dbb982d
David Mansy <david.mansy@me.com>
2013-09-23 08:57:34 +0200

    Include a copy of hackreactor chatbuilder

--- chatter-server.js
+++ chatter-server.js
@@ -6,7 +6,7 @@ var app = require("express")()
   , chatters = []
   , fs = require('fs');
 
-eval(fs.readFileSync('hackreactor_chatbuilder')+'');
+eval(fs.readFileSync('./hackreactor_chatbuilder')+'');
 
 //Express
 server.listen(8080);
